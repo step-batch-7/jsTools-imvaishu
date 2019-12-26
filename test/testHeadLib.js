b242915c11b00c53e04dd30a4757d43e02e1ca09
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2019-12-26 17:22:55 +0530

    added extra unit test cases to check head.js with option

--- test/testHeadLib.js
+++ test/testHeadLib.js
@@ -61,4 +61,64 @@ describe("head", function() {
 
     assert.deepStrictEqual(head(args, fs), expectedAns);
   });
+
+  it("should return array of lines if file contains less no. of line than mentioned in range and -n option given", function() {
+    const args = ["-n", "9", "path"];
+
+    const existsSync = function(path) {
+      assert.strictEqual(path, "path");
+      return true;
+    };
+
+    const readFileSync = function(path, encoder) {
+      assert.strictEqual(path, "path");
+      assert.strictEqual(encoder, "utf-8");
+      return `0\n1\n2\n3\n4\n5`;
+    };
+
+    const fs = { readFileSync, existsSync };
+    const expectedAns = { content: `0\n1\n2\n3\n4\n5`, error: "" };
+
+    assert.deepStrictEqual(head(args, fs), expectedAns);
+  });
+
+  it("should return array of lines mentioned in range when -n option given", function() {
+    const args = ["-n", "2", "path"];
+
+    const existsSync = function(path) {
+      assert.strictEqual(path, "path");
+      return true;
+    };
+
+    const readFileSync = function(path, encoder) {
+      assert.strictEqual(path, "path");
+      assert.strictEqual(encoder, "utf-8");
+      return `0\n1\n2\n3\n4\n5`;
+    };
+
+    const fs = { readFileSync, existsSync };
+    const expectedAns = { content: `0\n1`, error: "" };
+
+    assert.deepStrictEqual(head(args, fs), expectedAns);
+  });
+
+  it("should return array of lines mentioned in range when -n option given with no.", function() {
+    const args = ["-n2", "path"];
+
+    const existsSync = function(path) {
+      assert.strictEqual(path, "path");
+      return true;
+    };
+
+    const readFileSync = function(path, encoder) {
+      assert.strictEqual(path, "path");
+      assert.strictEqual(encoder, "utf-8");
+      return `0\n1\n2\n3\n4\n5`;
+    };
+
+    const fs = { readFileSync, existsSync };
+    const expectedAns = { content: `0\n1`, error: "" };
+
+    assert.deepStrictEqual(head(args, fs), expectedAns);
+  });
 });
